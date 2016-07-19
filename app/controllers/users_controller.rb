8f435d13f22cdc01c32eea462885d0a1aa265cd9
Marie Denney <mldenne@gmail.com>
2016-07-19 14:12:41 -0400

    First commit, added controller Users, gem files octokit, figaro, pry-rails, pgand rails_12factor

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+  def index
+    client = Octokit::Client.new(:access_token => ENV["octokit_token"])
+    @repos = client.search_repos(params[:character] || "wolverine")[:items]
+  end
+
+end
