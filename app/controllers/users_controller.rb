3e1b23ae73d16b460686986b3acb52cac68a9e94
Marie Denney <mldenne@gmail.com>
2016-07-19 17:06:46 -0400

    Finally, with help, figured out how to map over the data - yay

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
 
   def index
     client = Octokit::Client.new(:access_token => ENV["octokit_token"])
-    @repos = client.search_repos(params[:character] || "wolverine")[:items]
+    @users = client.search_users("location:Indianapolis language:Ruby", page: params[:page], sort: "joined", order: "asc")[:items]
+    @users = @users.map { |user| client.user(user.login) }
   end
 
 end
