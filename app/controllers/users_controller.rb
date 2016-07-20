c957c6581f81f092dd78dff712bdfdcd9cff5365
Marie Denney <mldenne@gmail.com>
2016-07-19 21:43:21 -0400

    Updated erb files with formatting and pagination, added stylesheet, updated gem pg file and database.yml with PostgreSQL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
     client = Octokit::Client.new(:access_token => ENV["octokit_token"])
     @users = client.search_users("location:Indianapolis language:Ruby", page: params[:page], sort: "joined", order: "asc")[:items]
     @users = @users.map { |user| client.user(user.login) }
-    @pages = 30.0).ceil
+    total_count = client.search_users("location:Indianapolis language:Ruby")[:total_count]
+    @pages = (total_count / 30.0).ceil
   end
 
 end
